b7a2d2152363c5ff06c7ee1a81b6298243835037
ABACADEMY-SAB <alihazoor222@gmail.com>
2026-08-23 08:14:02 +0500

    Refactor SQL queries in Student model methods

--- models/student.js
+++ models/student.js
@@ -9,36 +9,65 @@ class Student {
     // Get All Students
     static getAll(callback) {
         db.query(
-            "SELECT * FROM students ORDER BY id DESC",
+            `SELECT
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status
+             FROM students
+             ORDER BY id DESC`,
             callback
         );
     }
 
+
     // Get Student By ID
     static getById(id, callback) {
         db.query(
-            "SELECT * FROM students WHERE id = $1",
+            `SELECT
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status
+             FROM students
+             WHERE id = $1`,
             [id],
             callback
         );
     }
 
+
     // Get Student By Username
     static getByUsername(username, callback) {
         db.query(
-            "SELECT * FROM students WHERE username = $1",
+            `SELECT *
+             FROM students
+             WHERE username = $1`,
             [username],
             callback
         );
     }
 
+
     // Create Student
     static create(data, callback) {
+
         db.query(
             `INSERT INTO students
-            (username, password, fullname, email, phone)
-            VALUES ($1,$2,$3,$4,$5)
-            RETURNING *`,
+                (username, password, fullname, email, phone)
+             VALUES
+                ($1, $2, $3, $4, $5)
+             RETURNING
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status`,
             [
                 data.username,
                 data.password,
@@ -48,10 +77,13 @@ class Student {
             ],
             callback
         );
+
     }
+
 
     // Update Student
     static update(id, data, callback) {
+
         db.query(
             `UPDATE students
              SET
@@ -59,7 +91,13 @@ class Student {
                 email = $2,
                 phone = $3
              WHERE id = $4
-             RETURNING *`,
+             RETURNING
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status`,
             [
                 data.fullname,
                 data.email,
@@ -68,33 +106,79 @@ class Student {
             ],
             callback
         );
+
     }
+
+
+    // Change Password
+    static changePassword(id, password, callback) {
+
+        db.query(
+            `UPDATE students
+             SET password = $1
+             WHERE id = $2`,
+            [
+                password,
+                id
+            ],
+            callback
+        );
+
+    }
+
 
     // Delete Student
     static delete(id, callback) {
+
         db.query(
-            "DELETE FROM students WHERE id = $1",
+            `DELETE FROM students
+             WHERE id = $1`,
             [id],
             callback
         );
+
     }
+
 
     // Block Student
     static block(id, callback) {
+
         db.query(
-            "UPDATE students SET status = 'blocked' WHERE id = $1 RETURNING *",
+            `UPDATE students
+             SET status = 'blocked'
+             WHERE id = $1
+             RETURNING
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status`,
             [id],
             callback
         );
+
     }
+
 
     // Unblock Student
     static unblock(id, callback) {
+
         db.query(
-            "UPDATE students SET status = 'active' WHERE id = $1 RETURNING *",
+            `UPDATE students
+             SET status = 'active'
+             WHERE id = $1
+             RETURNING
+                id,
+                username,
+                fullname,
+                email,
+                phone,
+                status`,
             [id],
             callback
         );
+
     }
 
 }
