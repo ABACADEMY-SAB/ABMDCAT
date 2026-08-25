a4060feeb74803368b78b532440bfbcb28e19b4d
ABACADEMY-SAB <alihazoor222@gmail.com>
2026-08-25 12:34:34 +0500

    Rename testQuestion controller import

--- routes/testQuestion.js
+++ routes/testQuestion.js
@@ -7,7 +7,7 @@ const express = require("express");
 const router = express.Router();
 
 const testQuestionController =
-    require("../controllers/testQuestion");
+    require("../controllers/testQuestionController");
 
 
 // =====================================
